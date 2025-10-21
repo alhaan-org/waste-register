de05ca2e65025eb7f2cd306624d2d7b8b6807693
alhaan-org <alhaanahmed@nexiatech.org>
2025-10-21 23:20:13 +0500

    feat: Just small commit, bored today

--- e_waste_tracker/warehouse/views.py
+++ e_waste_tracker/warehouse/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def return_warehouse_dashboard(request):
+    return HttpResponse("Warehouse Dashboard Page")
