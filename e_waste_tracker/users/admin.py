38ea39da927de9213eff045176b6f8c336c74cfa
alhaan-org <alhaanahmed@nexiatech.org>
2025-10-23 23:18:48 +0500

    feat: Added User model, registered in Admin, Next step is to validate user and implement authentication and redirecting to warehouse

--- e_waste_tracker/users/admin.py
+++ e_waste_tracker/users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'is_staff', 'is_active')
--- e_waste_tracker/users/models.py
+++ e_waste_tracker/users/models.py
@@ -1,4 +1,21 @@
 from django.db import models
-# from django
-# Create your models here.
-# class CustomUser()
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    first_name = None
+    last_name = None
+
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=12, unique=True)
+    cnic_no = models.CharField(max_length=15, unique=True)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.username
+
+
+    
+
