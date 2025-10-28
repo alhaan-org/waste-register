82f61e9a55183ab44f971b042f72acac5fa481f3
alhaan-org <alhaanahmed@nexiatech.org>
2025-10-28 20:56:38 +0500

    feat: Setup Warehouse Pages (Templates)

--- e_waste_tracker/warehouse/views.py
+++ e_waste_tracker/warehouse/views.py
@@ -5,5 +5,20 @@ from django.http import HttpResponse
 
 class WareHouseView(View):
     def get(self, request):
-        return HttpResponse("This is Warehouse Page")
+        return render(request, "warehouse/dashboard.html")
 
+class WareHouseStatus(View):
+    def get(self, request):
+        return render(request, "warehouse/status.html")
+
+class WareHouseCheckInOut(View):
+    def get(self, request):
+        return render(request, "warehouse/checkinout.html")
+
+class WareHouseRevenueView(View):
+    def get(self, request):
+        return render(request, "warehouse/revenue.html")
+
+class WareHouseSettingsView(View):
+    def get(self, request):
+        return render(request, "warehouse/settings.html")
