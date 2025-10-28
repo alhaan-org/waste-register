735a5d90ec74c5158082b868de2cdb699cc2a26d
alhaan-org <alhaanahmed@nexiatech.org>
2025-10-28 20:58:04 +0500

    feat: Urls added for Warehouse Views

--- e_waste_tracker/warehouse/urls.py
+++ e_waste_tracker/warehouse/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = "warehouse"
 
 urlpatterns = [
-    path("home", views.WareHouseView.as_view(), name="home")
+    path("home/", views.WareHouseView.as_view(), name="home"),
+    path("home/status/", views.WareHouseStatus.as_view(), name="status"),
+    path("home/checkinout/", views.WareHouseCheckInOut.as_view(), name="checkinout"),
+    path("home/revenue/", views.WareHouseRevenueView.as_view(), name="revenue"),
+    path("home/settings/", views.WareHouseSettingsView.as_view(), name="settings")
 ]
